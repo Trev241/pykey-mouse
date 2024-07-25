69d9f9dd7ca169ce444258d5b50920a3828e36a0
Trevis Liu <trevisliu@gmail.com>
2024-07-25 12:56:04 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+
+from pynput.keyboard import Key, Listener
+from pynput.mouse import Controller
+
+SENSITIVITY = 5
+INPUT_KEYS = set([Key.left, Key.right, Key.up, Key.down])
+X_OFFSET = {Key.left: -1, Key.right: 1}
+Y_OFFSET = {Key.up: -1, Key.down: 1}
+
+
+class InputMapper:
+    def __init__(self):
+        self.keys_down = set()
+        self.dx = 0
+        self.dy = 0
+
+        self.mouse = Controller()
+
+    def update(self):
+        self.mouse.move(self.dx * SENSITIVITY, self.dy * SENSITIVITY)
+
+    def on_press(self, key):
+        if key not in INPUT_KEYS:
+            return
+
+        if key not in self.keys_down:
+            self.keys_down.add(key)
+            self.dx += X_OFFSET.get(key, 0)
+            self.dy += Y_OFFSET.get(key, 0)
+
+    def on_release(self, key):
+        if key in INPUT_KEYS:
+            self.keys_down.remove(key)
+            self.dx -= X_OFFSET.get(key, 0)
+            self.dy -= Y_OFFSET.get(key, 0)
+
+
+input_mapper = InputMapper()
+with Listener(
+    on_press=input_mapper.on_press, on_release=input_mapper.on_release
+) as listener:
+    while True:
+        input_mapper.update()
+        time.sleep(0.0025)
+
+    listener.join()
